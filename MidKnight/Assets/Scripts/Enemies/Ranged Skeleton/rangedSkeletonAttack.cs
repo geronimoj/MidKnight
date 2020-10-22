a0c1c73a5684911d748192653cd30b85b8ae5e60
s200098 <s200098@ADL-A4A53C>
2020-10-22 16:48:10 +1030

    Worked on ranged skele atk

---
+++ MidKnight/Assets/Scripts/Enemies/Ranged Skeleton/rangedSkeletonAttack.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rangedSkeletonAttack : StateMachineBehaviour
+{
+    /// <summary>
+    /// Ranged skeleton attack
+    /// </summary>
+
+    public float startTimeTillAtk;
+    float timeTillAtk;
+    bool hasUsedAtk;
+
+    public float startTimeTillIdle;
+    float timeTillIdle;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //initialise stuff
+        timeTillAtk = startTimeTillAtk;
+        hasUsedAtk = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //attack after x seconds
+        if(timeTillAtk > 0)
+        {
+            timeTillAtk -= Time.deltaTime;
+        }
+        else if (!hasUsedAtk)
+        {
+            hasUsedAtk = true;
+        }
+
+        //return to idle after x seconds
+        if(timeTillIdle > 0)
+        {
+            timeTillIdle -= Time.deltaTime;
+        }
+        else
+        {
+            animator.SetTrigger("idle");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
