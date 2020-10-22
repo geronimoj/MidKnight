727030d40967245b49ed13b101e59bfe1c8f67d9
s200098 <s200098@ADL-A4A53C>
2020-10-22 15:24:08 +1030

    changed king rat to large rat

--- MidKnight/Assets/Scripts/Enemies/Large Rat/largeRatIdle.cs
+++ MidKnight/Assets/Scripts/Enemies/Large Rat/largeRatIdle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kingRatIdle : StateMachineBehaviour
+public class largeRatIdle : StateMachineBehaviour
 {
     Transform ratTrans;
     Vector3 destination;
