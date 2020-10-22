2022e69c8494a84d7315858c20dbdc32307b6520
Luke Jones <nnblc565@gmail.com>
2020-10-22 16:06:24 +1030

    Changed Airborne movement to add a floaty-ness and holding the button results in a higher jump

--- MidKnight/Assets/Scripts/Player/StateMachine/States/AirborneMove.cs
+++ MidKnight/Assets/Scripts/Player/StateMachine/States/AirborneMove.cs
@@ -5,11 +5,50 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AirMove", menuName = "States/AirMove", order = 2)]
 public class AirborneMove : State
 {
-    public override void StateUpdate(ref PlayerController c)
-    {   //Get the input
-        float x = Input.GetAxisRaw("Horizontal");
-        float speed = 0;
+    public float maxFallSpeed = 0;
+    [Tooltip("How long the player can hold the jump key to not be affected by gravity")]
+    public float jumpHoldTime = 0;
+    [Tooltip("How long the player floats for after 'exiting' a jump")]
+    public float floatTime = 0;
 
+    private bool holdingJump = false;
+
+    private float jumpTimer = 0;
+    private float floatTimer = 0;
+
+    public override void StateStart(ref PlayerController c)
+    {
+        holdingJump = false;
+        jumpTimer = 0;
+        floatTimer = 0;
+        if (Input.GetAxisRaw("Jump") > 0)
+        {
+            jumpTimer = jumpHoldTime;
+            floatTimer = floatTime;
+            holdingJump = true;
+        }
+    }
+
+    public override void StateUpdate(ref PlayerController c)
+    {   //Get the inputs
+        float x = Input.GetAxisRaw("Horizontal");
+
+        if (jumpTimer > 0 && holdingJump && Input.GetAxisRaw("Jump") > 0)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+        else if (holdingJump)
+        {
+            c.movement.VertSpeed = 0;
+            floatTimer -= Time.deltaTime;
+            jumpTimer = 0;
+        }
+
+        if (jumpTimer <= 0 && floatTimer <= 0)
+            holdingJump = false;
+
+        //Find the players speed
+        float speed = 0;
         if (x != 0)
             speed = c.MoveSpeed;
         //Get the direction to the right
@@ -23,7 +62,13 @@ public class AirborneMove : State
         c.movement.Direction = right;
         //Set the players speed
         c.movement.HozSpeed = speed;
-        c.movement.VertSpeed -= c.Gravity * Time.deltaTime;
+
+        if (!holdingJump)
+            c.movement.VertSpeed -= c.Gravity * Time.deltaTime;
+        //Cap the players fall speed if necessary
+        if (maxFallSpeed > 0)
+            c.movement.VertSpeed = Mathf.Clamp(c.movement.VertSpeed, -maxFallSpeed, Mathf.Infinity);
+
         //Move the character
         c.Move(c.movement.MoveVec * Time.deltaTime);
     }
