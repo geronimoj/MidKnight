305894c51e98f8c945071665edbee3d69336b218
Leonard Brinkworth <s181696@students.aie.edu.au>
2020-10-23 10:52:07 +1030

    Unlockables added.

---
+++ MidKnight/Assets/Scripts/Unlocks/Unlockable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unlockable : MonoBehaviour
+{
+    public string powerUP = "new moon";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch(powerUP)
+        {
+            case "new moon":
+                other.gameObject.GetComponent<UnlockTracker>().AddNewMoon();
+                Destroy(gameObject);
+                break;
+            case "crescent moon":
+                other.gameObject.GetComponent<UnlockTracker>().AddCrescentMoon();
+                Destroy(gameObject);
+                Debug.Log("crescent moon");
+                break;
+            case "half moon":
+                other.gameObject.GetComponent<UnlockTracker>().AddHalfMoon();
+                Destroy(gameObject);
+                break;
+            case "dash":
+                other.gameObject.GetComponent<UnlockTracker>().AddDash();
+                Destroy(gameObject);
+                break;
+            case "full moon":
+                other.gameObject.GetComponent<UnlockTracker>().AddFullMoon();
+                Destroy(gameObject);
+                break;
+            case "double jump":
+                other.gameObject.GetComponent<UnlockTracker>().AddDoubleJump();
+                Destroy(gameObject);
+                break;
+            case "moon beam":
+                other.gameObject.GetComponent<UnlockTracker>().AddMoonBeam();
+                Destroy(gameObject);
+                break;
+            case "eclipse":
+                other.gameObject.GetComponent<UnlockTracker>().AddEclipse();
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
