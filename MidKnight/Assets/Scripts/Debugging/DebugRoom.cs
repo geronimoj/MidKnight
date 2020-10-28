5f9037773ebc046d959247c4fab292b7cd3e4310
Luke Jones <nnblc565@gmail.com>
2020-10-28 11:05:12 +1030

    Commented the DebugRoom script

--- MidKnight/Assets/Scripts/Debugging/DebugRoom.cs
+++ MidKnight/Assets/Scripts/Debugging/DebugRoom.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DebugRoom : MonoBehaviour
-{
+{   
+    /// <summary>
+    /// The room you want to debug
+    /// </summary>
+    [Tooltip("The room you want to debug")]
     public Room room;
+    /// <summary>
+    /// The radius of the pathNodes
+    /// </summary>
     public float pointRadius;
 #if !UNITY_EDITOR
     private void Awake()
@@ -12,16 +19,19 @@ public class DebugRoom : MonoBehaviour
         Destroy(this);
     }
 #endif
-
+    /// <summary>
+    /// Draws the Path and PathNodes
+    /// </summary>
     private void OnDrawGizmos()
-    {
+    {   //If we don't have a room, don't bother
         if (room == null)
             return;
+        //Loop through the pathNodes and display them
         for (int i = 0; i < room.pathNodes.Length; i++)
-        {
+        {   //Draw the pathNodes with a colour
             Gizmos.color = Color.blue;
             Gizmos.DrawWireSphere(PNodeToV3(room.pathNodes[i]), pointRadius);
-
+            //If we have a segment, draw the segment
             if (i < room.pathNodes.Length - 1)
             {
                 Gizmos.color = Color.red;
@@ -29,7 +39,11 @@ public class DebugRoom : MonoBehaviour
             }
         }
     }
-
+    /// <summary>
+    /// Converts the cords of a PathNode to Vector3 cords
+    /// </summary>
+    /// <param name="v">The PathNode to convert</param>
+    /// <returns>The PathNodes cords as a V3</returns>
     private Vector3 PNodeToV3 (Vector2 v)
     {
         return new Vector3(v.x, 0, v.y);
