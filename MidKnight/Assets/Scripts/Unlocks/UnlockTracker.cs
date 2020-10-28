495840d7f0dd06e61c7fbfeedb2969d99b2f9354
Leonard Brinkworth <s181696@students.aie.edu.au>
2020-10-28 11:19:39 +1030

    Added unityeditor code to see things.

--- MidKnight/Assets/Scripts/Unlocks/UnlockTracker.cs
+++ MidKnight/Assets/Scripts/Unlocks/UnlockTracker.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class UnlockTracker : MonoBehaviour
 {
-    [SerializeField]
-    private Dictionary<string, bool> unlocks = new Dictionary<string, bool>();
+    Dictionary<string, bool> unlocks = new Dictionary<string, bool>();
 #if UNITY_EDITOR
-
+    public string[] stringArray;
+    public bool[] boolArray;
 #endif
     public void SetKey(string key, bool value)
     {
@@ -17,12 +17,39 @@ public class UnlockTracker : MonoBehaviour
             {
                 //unlocks.Remove(key);
                 unlocks[key] = value;
-                break;
+#if UNITY_EDITOR
+                int e = 0;
+
+                foreach (KeyValuePair<string, bool> kvp in unlocks)
+                {
+                    if (key == kvp.Key)
+                    {
+                        boolArray[e] = value;
+                        break;
+                    }
+
+                    e++;
+                }
+#endif
+                return;
             }
         }
 
         unlocks.Add(key, value);
-    }
+#if UNITY_EDITOR
+        stringArray = new string[unlocks.Count()];
+        boolArray = new bool[unlocks.Count()];
+
+        int i = 0;
+
+        foreach (KeyValuePair<string, bool> KVP in unlocks)
+        {
+            stringArray[i] = KVP.Key;
+            boolArray[i] = KVP.Value;
+            i++;
+        }
+#endif
+        }
     public bool GetKeyValue(string key)
     {
         foreach(KeyValuePair<string, bool> KVP in unlocks)
