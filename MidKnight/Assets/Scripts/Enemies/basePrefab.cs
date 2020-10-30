9c2d035bd9b5761316dd3df2473e38150ca7984e
s200098 <s200098@ADL-A4A53C>
2020-10-30 14:51:53 +1030

    inplemented skills and enemy hitbox dealing damage to the player

--- MidKnight/Assets/Scripts/Enemies/basePrefab.cs
+++ MidKnight/Assets/Scripts/Enemies/basePrefab.cs
@@ -8,7 +8,9 @@ public class basePrefab : MonoBehaviour
     [HideInInspector] public Transform enemyTrans;
     [HideInInspector] public Transform prefabTrans;
     [HideInInspector] public Vector3 destination = new Vector3(0,0,0);
+    Character player;
     public int speed = 1;
+    public int damage = 1;
 
     // Start is called before the first frame update
     public virtual void Start()
@@ -16,8 +18,9 @@ public class basePrefab : MonoBehaviour
         playerTrans = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         enemyTrans = GetComponent<characterOwner>().Owner.GetComponent<Transform>();
         prefabTrans = GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
 
-        if(PlayerIsOnRight())
+        if (PlayerIsOnRight())
         {
             FaceRight();
         }
@@ -38,6 +41,16 @@ public class basePrefab : MonoBehaviour
             return false;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            player.TakeDamage(damage);
+            Debug.Log("Skill did damage");
+        }
+    }
+
 
     /// <summary>
     /// Make the enemy move to this destination
---
+++ MidKnight/Assets/Scripts/EnemyHitbox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitbox : MonoBehaviour
+{
+    Character player;
+    Enemy enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+        enemy = GetComponentInParent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            player.TakeDamage(enemy.damage);
+            Debug.Log("Enemy did damage");
+        }
+    }
+}
