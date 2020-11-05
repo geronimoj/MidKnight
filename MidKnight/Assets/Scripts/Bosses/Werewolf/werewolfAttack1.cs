f6a83322317647af8fe159cc6c6f31a8b3e4b3db
s200098 <s200098@ADL-A4A53C>
2020-11-05 15:37:48 +1030

    completed werewolf

---
+++ MidKnight/Assets/Scripts/Bosses/Werewolf/werewolfAttack1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class werewolfAttack1 : baseBossAttack
+{
+    public int noOfRocks;
+    Vector3 spawnPos;
+    public int phase2speed;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        destination.Set((arenaLeftXCoordinate + arenaRightXCoordinate) / 2, enemyTrans.position.y + 0.1f, enemyTrans.position.z);
+        
+        if(animator.GetComponent<Enemy>().isPhase2)
+        {
+            speed = phase2speed;
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        MoveToDestination(destination);
+
+        if (timeTillAtk > 0)
+        {
+            timeTillAtk -= Time.deltaTime;
+        }
+        else if (!hasUsedMove)
+        {
+            hasUsedMove = true;
+
+            for (int i = 0; i < noOfRocks; i++)
+            {
+                spawnPos = new Vector3(Random.Range(arenaLeftXCoordinate, arenaRightXCoordinate), Random.Range(arenaUpYCoordinate + 5, arenaUpYCoordinate + 20), enemyTrans.position.z);
+                Instantiate(attack, spawnPos, enemyTrans.rotation);
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
