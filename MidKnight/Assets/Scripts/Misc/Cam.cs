0e26a7e674a8d63918eaf14c4fcde4dfc640715d
Alex Eime <s200101@students.aie.edu.au>
2020-11-05 15:26:20 +1030

    Created and attached new camera script, allows adjusting of x,y,z offset, added smooth camera

---
+++ MidKnight/Assets/Scripts/Misc/Cam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public Transform player;
+
+    //Allows us to adjust the X, Y and Z values in unity
+    public Vector3 cameraOffset;
+
+    //Can adjust cameraSpeed to suit,
+    //lower numbers will make the camera more "floaty" higher numbers are a more "rigid" camera,
+    //can also be changed in unity
+    public float cameraSpeed = 0.1f;
+
+    void Start()
+    {
+        transform.position = player.position + cameraOffset;
+    }
+
+    void Update()
+    {
+        Vector3 finalPosition = player.position + cameraOffset;
+        Vector3 currentPosition = Vector3.Lerp(transform.position, finalPosition, cameraSpeed);
+        transform.position = currentPosition;
+    }
+}
