a40c2ea4de1a5c475d50e5454235615637a2a90d
Leonard Brinkworth <s181696@students.aie.edu.au>
2020-11-05 13:27:28 +1030

    Finished basic framework. Gonna start importing it to sprint 2 branch.

--- MidKnight/Assets/Scripts/Managers/MenuManager.cs
+++ MidKnight/Assets/Scripts/Managers/MenuManager.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
@@ -14,15 +14,26 @@ public class MenuManager : MonoBehaviour
     public GameObject optionsMenu;
     public GameObject controlMenu;
     public AudioMixer audioMixer;
-    public TMP_Dropdown resolutionDropdown;
-    //public Dropdown resolutionDropdown;
-    Resolution[] resolutions;
+    private Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    public Toggle fullScreenToogle;
+    public Dropdown presetQualityDropdown;
+    //public Slider pixelLightCountSlider;
+    //public Text pixelLightCountText;
+    ///public Dropdown textureQualityDropdown;
+    ///public Dropdown anisotropicTexturesDropdown;
+    //public Dropdown antiAliasingDropdown;
+    //public Toggle softParticlesToogle;
+    ///public Toggle realtimeReflectionProbesToogle;
+    public Slider volumeSlider;
+    public Text volumeText;
+    private float currentVolume;
 
     private void Start()
     {
-        resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
         int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++)
@@ -37,8 +48,8 @@ public class MenuManager : MonoBehaviour
         }
 
         resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
     }
 
     private void Update()
@@ -113,24 +124,200 @@ public class MenuManager : MonoBehaviour
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
+        resolutionDropdown.value = resolutionIndex;
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-        Debug.Log($"Resolution: {Screen.width} x {Screen.height}");
     }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        fullScreenToogle.SetIsOnWithoutNotify(isFullScreen);
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetPresetQuality(int qualityIndex)
+    {
+        if (qualityIndex < 6)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+            //pixelLightCountSlider.value = QualitySettings.pixelLightCount;
+            //pixelLightCountText.text = $"{QualitySettings.pixelLightCount}";
+            ///textureQualityDropdown.value = QualitySettings.masterTextureLimit;
+            ///anisotropicTexturesDropdown.value = Convert.ToInt32(QualitySettings.anisotropicFiltering);
+            //antiAliasingDropdown.value = QualitySettings.antiAliasing;
+            //softParticlesToogle.SetIsOnWithoutNotify(QualitySettings.softParticles);
+            ///realtimeReflectionProbesToogle.SetIsOnWithoutNotify(QualitySettings.realtimeReflectionProbes);
+        }
+
+        presetQualityDropdown.value = qualityIndex;
+    }
+
+    /*public void SetPixelLightCount(float plcIndex)
+    {
+        QualitySettings.SetQualityLevel(6);
+        presetQualityDropdown.value = 6;
+        pixelLightCountSlider.value = plcIndex;
+        pixelLightCountText.text = $"{plcIndex}";
+        QualitySettings.pixelLightCount = (int)plcIndex;
+    }*/
+
+    /*public void SetTextureQuality(int textureIndex)
+    {
+        QualitySettings.SetQualityLevel(6);
+        presetQualityDropdown.value = 6;
+        textureQualityDropdown.value = textureIndex;
+        QualitySettings.masterTextureLimit = textureIndex;
+    }*///////////////////////////////////////////////////////////////////////////////////
+
+    /*public void SetAnisotropicTextures(int atIndex)
+    {
+        if (atIndex < 3)
+        {
+            QualitySettings.SetQualityLevel(6);
+            presetQualityDropdown.value = 6;
+            anisotropicTexturesDropdown.value = atIndex;
+
+            if (atIndex == 0)
+            {
+                QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+            }
+            else if (atIndex == 1)
+            {
+                QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
+            }
+            else if (atIndex == 2)
+            {
+                QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+            }
+        }
+    }*///////////////////////////////////////////////////////////////////////////////////
+
+    /*public void SetAntiAliasing(int aaIndex)
+    {
+        QualitySettings.SetQualityLevel(6);
+        presetQualityDropdown.value = 6;
+        antiAliasingDropdown.value = aaIndex;
+        QualitySettings.antiAliasing = aaIndex;
+    }*/
+
+    /*public void SetSoftParticles(bool isSoftParticles)
+    {
+        QualitySettings.SetQualityLevel(6);
+        presetQualityDropdown.value = 6;
+        softParticlesToogle.SetIsOnWithoutNotify(isSoftParticles);
+        QualitySettings.softParticles = isSoftParticles;
+    }*/
+
+    /*public void SetRealtimeReflectionProbes(bool isRealtimeReflectionProbes)
+    {
+        QualitySettings.SetQualityLevel(6);
+        presetQualityDropdown.value = 6;
+        realtimeReflectionProbesToogle.SetIsOnWithoutNotify(isRealtimeReflectionProbes);
+        QualitySettings.realtimeReflectionProbes = isRealtimeReflectionProbes;
+    }*///////////////////////////////////////////////////////////////////////////////////
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
+        currentVolume = volume;
+        volumeText.text = $"{currentVolume + 100}";
     }
 
-    public void SetQuality(int qualityIndex)
+    public void SaveSettings()
     {
-        QualitySettings.SetQualityLevel(qualityIndex);
-        Debug.Log($"Quality: {QualitySettings.GetQualityLevel()}");
+        PlayerPrefs.SetInt("Resolution", resolutionDropdown.value);
+        PlayerPrefs.SetInt("FullScreen", Convert.ToInt32(Screen.fullScreen));
+        PlayerPrefs.SetInt("PresetQuality", presetQualityDropdown.value);
+        //PlayerPrefs.SetFloat("PixelLightCount", pixelLightCountSlider.value);
+        ///PlayerPrefs.SetInt("TextureQuality", textureQualityDropdown.value);
+        ///PlayerPrefs.SetInt("AnisotropicTextures", anisotropicTexturesDropdown.value);
+        //PlayerPrefs.SetInt("AntiAliasing", antiAliasingDropdown.value);
+        PlayerPrefs.SetInt("SoftParticles", Convert.ToInt32(QualitySettings.softParticles));
+        PlayerPrefs.SetInt("RealtimeReflectionProbes", Convert.ToInt32(QualitySettings.realtimeReflectionProbes));
+        PlayerPrefs.SetFloat("Volume", currentVolume);
     }
 
-    public void SetFullScreen(bool isFullScreen)
+    public void LoadSettings(int currentResolutionIndex)
     {
-        Screen.fullScreen = isFullScreen;
+        if (PlayerPrefs.HasKey("Resolution"))
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt("Resolution");
+        }
+        else
+        {
+            resolutionDropdown.value = currentResolutionIndex;
+        }
+        if (PlayerPrefs.HasKey("FullScreen"))
+        {
+            fullScreenToogle.SetIsOnWithoutNotify(Convert.ToBoolean(PlayerPrefs.GetInt("FullScreen")));
+        }
+        else
+        {
+            fullScreenToogle.SetIsOnWithoutNotify(Screen.fullScreen);
+        }
+        if (PlayerPrefs.HasKey("PresetQuality"))
+        {
+            presetQualityDropdown.value = PlayerPrefs.GetInt("PresetQuality");
+        }
+        else
+        {
+            presetQualityDropdown.value = 3;
+        }
+        /*if (PlayerPrefs.HasKey("PixelLightCount"))
+        {
+            pixelLightCountSlider.value = PlayerPrefs.GetFloat("PixelLightCount");
+        }
+        else
+        {
+            pixelLightCountSlider.value = QualitySettings.pixelLightCount;
+        }*/
+        /*if (PlayerPrefs.HasKey("TextureQuality"))
+        {
+            textureQualityDropdown.value = PlayerPrefs.GetInt("TextureQuality");
+        }
+        else
+        {
+            textureQualityDropdown.value = 0;
+        }*///////////////////////////////////////////////////////////////////////////////////
+        /*if (PlayerPrefs.HasKey("AnisotropicTextures"))
+        {
+            anisotropicTexturesDropdown.value = PlayerPrefs.GetInt("AnisotropicTextures");
+        }
+        else
+        {
+            anisotropicTexturesDropdown.value = 0;
+        }*///////////////////////////////////////////////////////////////////////////////////
+        /*if (PlayerPrefs.HasKey("AntiAliasing"))
+        {
+            antiAliasingDropdown.value = PlayerPrefs.GetInt("AntiAliasing");
+        }
+        else
+        {
+            antiAliasingDropdown.value = 1;
+        }*/
+        /*if (PlayerPrefs.HasKey("SoftParticles"))
+        {
+            softParticlesToogle.SetIsOnWithoutNotify(Convert.ToBoolean(PlayerPrefs.GetInt("SoftParticles")));
+        }
+        else
+        {
+            softParticlesToogle.SetIsOnWithoutNotify(QualitySettings.softParticles);
+        }*/
+        /*if (PlayerPrefs.HasKey("RealtimeReflectionProbes"))
+        {
+            realtimeReflectionProbesToogle.SetIsOnWithoutNotify(Convert.ToBoolean(PlayerPrefs.GetInt("RealtimeReflectionProbes")));
+        }
+        else
+        {
+            realtimeReflectionProbesToogle.SetIsOnWithoutNotify(QualitySettings.realtimeReflectionProbes);
+        }*///////////////////////////////////////////////////////////////////////////////////
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+        }
+        else
+        {
+            volumeSlider.value = currentVolume;
+        }
     }
 
     public void Quit()
