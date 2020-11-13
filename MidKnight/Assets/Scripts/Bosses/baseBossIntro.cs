3d18364d50b0efee05699d28363bf722d536d633
s200098 <s200098@ADL-A4A53C>
2020-11-13 11:14:53 +1030

    added base boss intro to all bosses

---
+++ MidKnight/Assets/Scripts/Bosses/baseBossIntro.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseBossIntro : StateMachineBehaviour
+{
+    /// <summary>
+    /// Base boss intro script
+    /// </summary>
+    public float distanceFromPlayerToWake;
+    Transform playerTrans;
+    Transform enemyTrans;
+    bool isAwake = false;
+    public float timeTillIdle;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        enemyTrans = animator.GetComponent<Transform>();
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(Vector3.Distance(playerTrans.position, enemyTrans.position) < distanceFromPlayerToWake)
+        {
+            isAwake = true;
+        }
+
+        if(isAwake && timeTillIdle > 0)
+        {
+            timeTillIdle -= Time.deltaTime;
+        }
+        else if(timeTillIdle < 0)
+        {
+            animator.SetTrigger("idle");
+        }
+    }
+}
