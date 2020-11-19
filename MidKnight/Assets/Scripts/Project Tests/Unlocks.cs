06d89e1f5b7ccf9a7723b8f72ebad5a06a88653d
Luke Jones <nnblc565@gmail.com>
2020-11-19 13:06:47 +1030

    Started Work on the Unit Tests

--- MidKnight/Assets/Scripts/Project Tests/Unlocks.cs
+++ MidKnight/Assets/Scripts/Project Tests/Unlocks.cs
@@ -8,21 +8,40 @@ namespace Tests
 {
     public class Unlocks
     {
-        // A Test behaves as an ordinary method
         [Test]
-        public void UnlockTracker_Set_Get()
+        public void UnlockTracker_Set_Get_Values()
         {
-            // Use the Assert class to test conditions
+            UnlockTracker ut = CreateUnlockTracker();
+            //Check that require component is working
+            Debug.Assert(ut.gameObject.GetComponent<PlayerController>() != null);
+            //Set a key and check that its been added
+            ut.SetKey("NewKey", false);
+            Debug.Assert(ut.unlocks["NewKey"] == false);
+            //Update the Keys value and check that a new key wasn't added
+            ut.SetKey("NewKey", true);
+            Debug.Assert(ut.unlocks.Count == 1);
+            Debug.Assert(ut.unlocks["NewKey"] == true);
+            //Add another Key to make sure it supports multiple keys
+            ut.SetKey("AnotherKey", true);
+            Debug.Assert(ut.unlocks.Count == 2);
+            Debug.Assert(ut.unlocks["AnotherKey"] == true);
+            //Test an invalid key
+            Debug.Assert(ut.GetKeyValue("NotAKey") == false);
+            //Test a valid key
+            Debug.Assert(ut.GetKeyValue("NewKey") == true);
+            //Check that the key is not case sensitive
+            Debug.Assert(ut.GetKeyValue("AnOtherKeY") == true);
         }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
         [UnityTest]
         public IEnumerator UnlocksWithEnumeratorPasses()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
             yield return null;
+        }
+
+        private UnlockTracker CreateUnlockTracker()
+        {
+            GameObject ut = GameObject.Instantiate(new GameObject(), new Vector3(0, 0, 0), Quaternion.identity);
+            return ut.AddComponent<UnlockTracker>();
         }
     }
 }
