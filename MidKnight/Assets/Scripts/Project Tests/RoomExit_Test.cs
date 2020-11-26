a90835c9f47acc63d1e81f4ea747a43c8bae5d08
Luke Jones <nnblc565@gmail.com>
2020-11-26 17:15:40 +1030

    Added RoomExit tests

--- MidKnight/Assets/Scripts/Project Tests/RoomExit_Test.cs
+++ MidKnight/Assets/Scripts/Project Tests/RoomExit_Test.cs
@@ -9,11 +9,55 @@ namespace Tests
     public class RoomExit_Test
     {
         [UnityTest]
-        public IEnumerator RoomExit_TestWithEnumeratorPasses()
+        public IEnumerator RoomExit_ChangeRoom()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
+            ScreenFader_Test.CreateScreenFader();
+            //Spawn a player
+            PlayerController p = Player.CreatePlayer();
+            p.transform.position = new Vector3(0, 10, 0);
+            //Get the gamemanager
+            GameManager gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+            //Spawn an exit
+            GameObject exit = CreateRoomExit();
+            //Spawn a room and assign it to the game manager
+            gm.room = CreateRoom();
+            //Spawn another room and assign it to exit
+            Room target = CreateRoom();
+            exit.GetComponent<RoomExit>().nextRoom = target;
+            target.roomID = "TargetID";
+
             yield return null;
+            //Once the collision occurs, check that the screenfader is fading in.
+            //Drop the player onto the exit to cause a collision
+            p.Move(Vector3.down * 0);
+            yield return new WaitForSeconds(5);
+            //Check that the rooms have been swapped over
+            Debug.Assert(gm.room.roomID == "TargetID");
+            //Check that the screen is fading out.
+        }
+
+        public static GameObject CreateRoomExit()
+        {
+            GameObject g = GameObject.Instantiate(new GameObject());
+
+            g.AddComponent<RoomExit>();
+            g.GetComponent<BoxCollider>().isTrigger = true;
+            g.name = "Room";
+
+            return g;
+        }
+
+        public static Room CreateRoom()
+        {
+            GameObject g = GameObject.Instantiate(new GameObject());
+
+            Room r =  g.AddComponent<Room>();
+            r.roomID = "Valid";
+
+            r.entrances = new Vector3[1];
+            r.pathNodes = new Vector2[0];
+
+            return r;
         }
     }
 }
