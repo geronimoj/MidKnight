03aa55bebf0735f3fc8194884e8ab63044b1fb77
Luke Jones <nnblc565@gmail.com>
2020-11-27 11:51:04 +1030

    RoomExitSpawner added

---
+++ MidKnight/Assets/Scripts/Unlocks/RoomExitSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomExitSpawner : MonoBehaviour
+{
+    /// <summary>
+    /// The ID of the unlock
+    /// </summary>
+    public string unlockID = "NULL";
+    /// <summary>
+    /// A reference to the roomExit to deactivate
+    /// </summary>
+    public GameObject roomExit;
+
+    void Update()
+    {
+        if (roomExit == null)
+            return;
+        //Checks if the ID has been unlocked
+        if (PlayerController.Player.ut.GetKeyValue(unlockID))
+            //Unlock the exit
+            roomExit.SetActive(true);
+        else
+            //Hide the exit
+            roomExit.SetActive(false);
+    }
+}
