900f0755cd79e5ba3a2ac92b5f6250c71431a19a
Joshua Tan <jaykid1jaykid1@gmail.com>
2020-11-29 03:38:28 +1030

    fixed laser beam

---
+++ MidKnight/Assets/Scripts/Enemies/Ranged Skeleton/laserBeam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserBeam : basePrefab
+{
+    /// <summary>
+    /// increase the size of the laser beam after x seconds
+    /// </summary>
+    public float timeTillSizeIncrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeTillSizeIncrease > 0)
+        {
+            timeTillSizeIncrease -= Time.deltaTime;
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(1, 1, 30);
+        }
+    }
+}
